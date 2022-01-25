f9619366f022f4a7902d4548754411b76643a341
Daryl Ando <ando.daryl@hotmail.com>
2022-01-25 21:20:56 +0000

    Add file for database API + book class

---
+++ assets/javascript/database.js
@@ -0,0 +1,11 @@
+class book {
+  constructor {
+    this.title = ""
+    this.totalPages = 0
+    this.authorList = []
+    this.subjectList = []
+    this.year = "1984"
+  }
+}
+
+export default database
