e0f4f0af187f0ad981223a50e166b3b3bd6f5081
Daryl Ando <ando.daryl@hotmail.com>
2022-01-25 22:31:20 +0000

    Update book class + variable names

--- assets/javascript/database.js
+++ assets/javascript/database.js
@@ -1,11 +1,14 @@
-class book {
-  constructor {
+class Book {
+  // ID system for books: pre-increment currentBookNumber & assign ID to new value of currentBookNumber
+  static currentBookNumber = 0
+  constructor() {
+    this.bookNumber = ++this.constructor.currentBookNumber
     this.title = ""
     this.totalPages = 0
     this.authorList = []
     this.subjectList = []
-    this.year = "1984"
+    this.year = 1984
   }
 }
 
-export default database
+export default databaseAPI
