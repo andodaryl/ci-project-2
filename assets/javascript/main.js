d2426c6e57182a7f6efdc0dc83722cc913a7b156
Daryl Ando <ando.daryl@hotmail.com>
2022-01-26 15:56:15 +0000

    Add basic bookList system + styling & fix js module errors

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,34 @@
-const databaseAPI = require('./database.js')
+import database from './modules/database.js'
+
+const bookList = document.querySelector('#bookList')
+const bookCard = document.querySelector('#addBookCard')
+const addBookForm = document.querySelector('#addBookCard form')
+
+function toggleVisibility(element) {
+  element.classList.toggle('hidden')
+}
+
+function addCard(event) {
+  event.preventDefault()
+  const bookNumber = 0
+  const clone = bookCard.cloneNode(true)
+  const submitBtn = clone.querySelector('[type="submit"]')
+  const resetBtn = clone.querySelector('[type="reset"]')
+  const containerBtn = clone.querySelector('.btn-container-action')
+  // Default states
+  clone.removeAttribute('id')
+  toggleVisibility(containerBtn)
+  submitBtn.innerText = 'Accept'
+  resetBtn.innerText = 'Cancel'
+  // Event listeners
+  clone.onmouseenter = function() {
+    toggleVisibility(containerBtn)
+  }
+  clone.onmouseleave = function() {
+    toggleVisibility(containerBtn)
+  }
+  // Add to DOM
+  bookList.appendChild(clone)
+}
+
+addBookForm.onsubmit = addCard
