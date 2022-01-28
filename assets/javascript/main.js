1ccbc093f437ad433220e833dc6cf433f69b2bee
Daryl Ando <ando.daryl@hotmail.com>
2022-01-28 15:11:29 +0000

    Remove all code except imports

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,15 +2,3 @@
 import CODE from './modules/dictionary.js' // Public Dictionary
 import databaseAPI from './modules/database.js'
 import displayAPI from './modules/display.js'
-
-// COMPONENTS
-// addBookBtn
-const addBookBtn = document.querySelector('#addBookBtn')
-addBookBtn.onclick = () => {
-  const newBook = displayAPI.createBookCard({
-    title: 'Yes'
-  })
-  newBook === CODE.STATUS_TYPE.FAILURE
-  ? console.error('Book not created')
-  : displayAPI.showBook(newBook)
-}
