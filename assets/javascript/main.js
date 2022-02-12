79763c095da59185508ab89435701374f50722b2
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 08:51:39 +0000

    Add bind component func

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,4 +2,13 @@
 import CODE from './modules/dictionary.js' // Public Dictionary
 import dataAPI from './modules/database.js'
 import displayAPI from './modules/display.js'
-import BookCard from './components/bookcard.js'
+import bindModalCreate from './components/modalcreate.js'
+
+const display = document.querySelector('#book-list')
+const bookModal = document.querySelector('#bookModal')
+const sampleBook = document.querySelector('[data-book-id="sampleBook"]')
+
+// Create Book Card
+const modalCreateBtn = document.querySelector('.btn-modal-activate[data-modal-action="add"]')
+const modalCreateAction = bindModalCreate({display, bookModal, sampleBook})
+modalCreateBtn.onclick = () => modalCreateAction()
