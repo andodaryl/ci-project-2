74720aa5a372f6d340fd4cbb325aafc3ce5208de
Daryl Ando <ando.daryl@hotmail.com>
2022-02-12 08:51:12 +0000

    Fix reference errors

--- assets/javascript/components/bookcard.js
+++ assets/javascript/components/bookcard.js
@@ -1,37 +1,40 @@
 // IMPORT
 import CODE from '../modules/dictionary.js'
 import displayAPI from '../modules/display.js'
+import dataAPI from '../modules/database.js'
+import bindModalEdit from './modaledit.js'
 
 // EXPORT book card generator
-export default (bookData) => {
+export default ({display, sampleBook, bookModal}) => (bookData) => {
    const RESULT = {
      STATUS: CODE.STATUS_TYPE.WAIT,
-     CONTENTS: null // BookCard
+     CONTENTS: null // BookCard Element
    }
    try {   
-     const BOOK = bookData
-     const safeBook = databaseAPI.getSafeData(BOOK, CODE.OBJ_TYPE.BOOK)
+     const checkSafeBook = dataAPI.getSafeBookList(bookData)
      // Exit early if data is unsafe
-     if (safeBook === CODE.STATUS_TYPE.FAILURE) throw 'Could not get safe data'
-     const bookId = safeBook[CODE.FIELD_TYPE.ID]
+     if (checkSafeBook.STATUS === CODE.STATUS_TYPE.FAILURE) throw 'Could not get safe book'
+     const safeBook = checkSafeBook.CONTENTS
+     const safeBookId = safeBook[CODE.FIELD_TYPE.ID]
      // Create component & target sub-components
      const clone = sampleBook.cloneNode(true)
      const actionBtnContainer = clone.querySelector('.action-btn-container')
      const deleteBtn = actionBtnContainer.querySelector('[data-action-type="delete"]')
      const editBtn = actionBtnContainer.querySelector('[data-action-type="edit"]')
      // Default component states
-     clone.setAttribute('data-book-id', bookId)
+     clone.setAttribute('data-book-id', safeBookId)
      clone.classList.toggle('d-none')
      clone.removeAttribute('aria-hidden')
-     databaseAPI.toggleVisibility(actionBtnContainer, true)
+     displayAPI.toggleVisibility(actionBtnContainer, true)
      // Component behaviours
      // Container
-     clone.onmouseenter = () => databaseAPI.toggleVisibility(actionBtnContainer, true)
-     clone.onmouseleave = () => databaseAPI.toggleVisibility(actionBtnContainer, false)
+     clone.onmouseenter = () => displayAPI.toggleVisibility(actionBtnContainer, true)
+     clone.onmouseleave = () => displayAPI.toggleVisibility(actionBtnContainer, false)
      // Delete Button
-     deleteBtn.onclick = () => displayAPI.hideBook(bookId)
+     deleteBtn.onclick = () => displayAPI.deleteBook(safeBookId)
      // Edit Button
-     editBtn.onclick = () => console.log('Edit modal activated')
+     const modalEditAction = bindModalEdit({display, sampleBook, bookModal})
+     editBtn.onclick = () => modalEditAction(safeBook)
     //  Update RESULT
     RESULT.CONTENTS = clone
     RESULT.STATUS = CODE.STATUS_TYPE.SUCCESS
